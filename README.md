612c8d6e6bc2c89266de2de4010998fa1ba64009
shrestu <umang.shrestha@autodesk.com>
2025-07-14 20:48:13 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Modulo Three
+
+## How to run the project locally
+Please install [UV](https://docs.astral.sh/uv/getting-started/installation/#standalone-installer) package manager.
+
+```sh
+uv venv --python 3.12
+source .venv/bin/activate
+uv sync
+```
+
+
+## Build the project
+```sh
+uv build
+```
+
+# Run unittest
+```sh
+uv run pytest
+```
---
+++ src/modulo_three/__init__.py
@@ -0,0 +1,20 @@
+"""ModuloThree package.
+
+This package provides a Finite State Automaton implementation for calculating
+the remainder when dividing a binary number by 3.
+
+Example:
+    >>> from modulo_three import mod_three
+    >>> mod_three("1101")  # 13 in decimal
+    1
+    >>> mod_three("1110")  # 14 in decimal
+    2
+    >>> mod_three("1111")  # 15 in decimal
+    0
+"""
+
+def modulo_three(binary_string: str) -> int:
+    return 0
+
+
+__version__ = "0.1.0" 
