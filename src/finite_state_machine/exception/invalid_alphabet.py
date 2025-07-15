3582ccb3da127eb3519047aab6326cf4943ad923
shrestu <umang.shrestha@autodesk.com>
2025-07-15 00:22:06 -0400

    refactor(exception): code cleanup

--- src/finite_state_machine/exception/invalid_alphabet.py
+++ src/finite_state_machine/exception/invalid_alphabet.py
@@ -1,13 +1,5 @@
-from ..state import FSMState
 from ..types import Alphabet
 from .base_exception import BaseException
-
-
-class InvalidFinalStateException(BaseException):
-    """ Exception raised for invalid final state. """
-    def __init__(self, state: FSMState, acceptable_final_states: list[FSMState]) -> None:
-        message = f"Invalid final state: {state}\nAcceptable final states: {acceptable_final_states}"
-        super().__init__(message)
 
 
 class InvalidAlphabetException(BaseException):
